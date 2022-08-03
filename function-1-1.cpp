4cc8d150e90f5095d012f1d5013299d05d7e1f89
Aleksa-Stojano <a1871019@student.adelaide.edu.au>
2022-08-03 14:04:49 +0930

    1-1

---
+++ function-1-1.cpp
@@ -0,0 +1,11 @@
+int sum_diagonal(int array[4][4]) {
+  int sum = 0;
+
+  for (int i = 0; i < 4; i++) {
+    for (int j = 0; j < 4; i++) {
+      if (j == i) {
+        sum += array[i][j];
+      }
+    }
+  }
+}
