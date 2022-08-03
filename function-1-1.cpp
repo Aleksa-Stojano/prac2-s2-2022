e1e23ab7954af2876003e12db65def95cba34505
Aleksa-Stojano <a1871019@student.adelaide.edu.au>
2022-08-03 14:17:42 +0930

    -1-1

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -1,13 +1,15 @@
+#include <iostream>
+
 int sum_diagonal(int array[4][4]) {
   int sum = 0;
 
   for (int i = 0; i < 4; i++) {
-    for (int j = 0; j < 4; i++) {
+    for (int j = 0; j < 4; j++) {
       if (j == i) {
         sum += array[i][j];
       }
     }
   }
-
+  std::cout << sum << std::endl;
   return sum;
 }
--- main-1-1.cpp
+++ main-1-1.cpp
@@ -1,5 +1,6 @@
-#include <iostream>
+#include <stdio.h>
 
+#include <iostream>
 using namespace std;
 extern int sum_diagonal(int array[4][4]);
 
