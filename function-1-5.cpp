87615afe1b1bbe81c9af6711b6e31aa88834fe16
Aleksa-Stojano <a1871019@student.adelaide.edu.au>
2022-08-03 19:51:49 +0930

    -1-5

---
+++ function-1-5.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#include <iostream>
+using namespace std;
+
+void print_summed(int array1[3][3], int array2[3][3]) {
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 3; j++) {
+      if (j == 2) {
+        printf("%d\n", (array1[i][j] + array2[i][j]));
+      } else {
+        printf("%d ", (array1[i][j] + array2[i][j]));
+      }
+    }
+  }
+  return;
+}
