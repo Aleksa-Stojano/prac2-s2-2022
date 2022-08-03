3270490262b54d860cfd7ee23b1c86921e981a0d
Aleksa-Stojano <a1871019@student.adelaide.edu.au>
2022-08-03 22:06:08 +0930

    -2-3 fornatting

--- function-2-3.cpp
+++ function-2-3.cpp
@@ -2,20 +2,20 @@
 #include <iostream>
 using namespace std;
 
-bool is_fanarray(int integers[], int length);
+bool is_palindrome(int integers[], int length);
 int sum_array_elements(int integers[], int length);
 
 int sum_if_palindrome(int integers[], int length) {
   if (length < 1) {
     return -1;
-  } else if (is_fanarray(integers, length) == true) {
+  } else if (is_palindrome(integers, length) == true) {
     int sum = sum_array_elements(integers, length);
     return sum;
   }
   return -2;
 }
 
-bool is_fanarray(int integers[], int length) {
+bool is_palindrome(int integers[], int length) {
   int half_n = floor(length / 2.0);
   for (int i = 0; i < half_n; i++) {
     if (integers[i] != integers[length - 1 - i]) {
