7f9b2ef81e318d84e79ae51ea6289c48d0845e63
Aleksa-Stojano <a1871019@student.adelaide.edu.au>
2022-08-12 15:07:42 +0930

    random stuff

--- main-1-3.cpp
+++ main-1-3.cpp
@@ -1,10 +1,14 @@
 
 #include <iostream>
 using namespace std;
-extern int count_digits(int array[4][4]);
+extern void count_digits(int array[4][4]);
 
 int main() {
-  int array[4][4] = {{0, 1, 2, 3}, {3, 2, 1, 0}, {3, 5, 6, 1}, {3, 8, 3, 4}};
+  int array[4][4] = {{0, 1, 2, 3}, 
+  {3, 2, 1, 0}, 
+  {3, 5, 6, 1}, 
+  {3, 8, 3, 4}};
+
   count_digits(array);
   return 0;
 }
